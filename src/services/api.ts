d9425a86dd4bb3c0151503e9739d1724574ade82
ArtemS <smoilovskyi151@gmail.com>
2022-02-01 13:20:39 +0100

    Changed address

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:4000' });
+const API = axios.create({ baseURL: 'https://tasks-ruby.herokuapp.com' });
 
 export default API;
